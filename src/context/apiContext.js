9bd2f6f45a9e5666ff1b37cf688167f5e03dcad8
AngeloIngaJP <85211590+AngeloIngaJP@users.noreply.github.com>
2022-08-12 21:32:59 -0500

    Update apiContext.js

--- src/context/apiContext.js
+++ src/context/apiContext.js
@@ -11,8 +11,8 @@ export const useApi = () => {
 };
 
 export const ApiProvider = ({ children }) => {
-  const url = "http://localhost:5000/api/";
-  /*  const url = process.env.REACT_APP_API; */
+  //const url = "http://localhost:5000/api/";
+  const url = process.env.REACT_APP_API; 
 
   const [token] = useAuth().token;
 
